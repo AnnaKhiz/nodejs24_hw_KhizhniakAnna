f52e0137e0fe46a5467eef39776f82b58736a6f1
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-24 17:39:32 +0200

    edit functions for more args

--- utils/logger.js
+++ utils/logger.js
@@ -1,14 +1,34 @@
+function checkData(args) {
+  let result = '';
+  const arr = [];
+  Object.values(args).forEach(el => {
+    if (typeof el !== 'object') {
+      result += el + ' ';
+    } else {
+      Object.values(el).forEach(element => {
+        arr.push(element);
+      })
+      result += arr.join(' ') + ' ';
+      arr.splice(0);
+    }
+  })
+  return result;
+}
+
 function logger(param) {
   return {
-    info: (text) => {
-      console.log(`${param}: ${text}`)
+    info: (...args) => {
+      const result = checkData(args);
+      console.log(`${param}: ${result}`);
     },
     warn: (...args) => {
-      console.warn(`You have a warn: ${args}`)
+      const result = checkData(args);
+      console.warn(`${param}: ${result}`);
     },
     error: (...args) => {
-      throw new Error(`You have an error: ${args}`)
-    }
+      const result = checkData(args);
+      console.error(`${param}: ${result}`);
+    },
   }
 }
 
