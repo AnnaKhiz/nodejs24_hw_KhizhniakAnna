e08b5e1b3b80158d6113299c805609932682e23e
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-24 20:18:52 +0200

    delete additional function

--- utils/logger.js
+++ utils/logger.js
@@ -1,34 +1,8 @@
-function checkData(args) {
-  let result = '';
-  const arr = [];
-  Object.values(args).forEach(el => {
-    if (typeof el !== 'object') {
-      result += el + ' ';
-    } else {
-      Object.values(el).forEach(element => {
-        arr.push(element);
-      })
-      result += arr.join(' ') + ' ';
-      arr.splice(0);
-    }
-  })
-  return result;
-}
-
 function logger(param) {
   return {
-    info: (...args) => {
-      const result = checkData(args);
-      console.log(`${param}: ${result}`);
-    },
-    warn: (...args) => {
-      const result = checkData(args);
-      console.warn(`${param}: ${result}`);
-    },
-    error: (...args) => {
-      const result = checkData(args);
-      console.error(`${param}: ${result}`);
-    },
+    info: (...args) => console.log(`${param}:`, ...args),
+    warn: (...args) => console.warn(`${param}:`, ...args),
+    error: (...args) => console.error(`${param}:`, ...args),
   }
 }
 
