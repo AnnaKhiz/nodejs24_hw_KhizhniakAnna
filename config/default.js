97c780171e74e94cb9e45a36dbeba74ed02fe6f0
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-27 17:46:30 +0200

    edit settings

--- config/default.js
+++ config/default.js
@@ -3,6 +3,6 @@ const logger = require('../utils/logger');
 const logColor = process.env.COLORS_ENABLED && process.env.COLORS_ENABLED == 1 ? 'enabled' : 'disabled';
 
 module.exports = {
-	logColor,
-	logger
+  logColor,
+  logger
 }
