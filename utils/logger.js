cb955d76ce3e38b4af4f22b05f6a358774d0670c
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-27 17:45:22 +0200

    edit settings

--- utils/logger.js
+++ utils/logger.js
@@ -1,23 +1,23 @@
 const colors = require('colors/safe');
 
 const logger = (param) => {
-	switch (process.env.LOG_LEVEL) {
-		case 'info': 
-			return {
-				info: (...args) => console.log(colors.bgWhite(`${param}:`), ...args),
-				warn: (...args) => console.warn(colors.bgYellow(`${param}:`), ...args),
-				error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
-			}
-		case 'error': 
-			return {
-				error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
-			}
-		default: 
-			return {
-				warn: (...args) => console.warn(colors.bgYellow(`${param}:`), ...args),
-				error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
-			}
-	}
+  switch (process.env.LOG_LEVEL) {
+    case 'info':
+      return {
+        info: (...args) => console.log(colors.bgWhite(`${param}:`), ...args),
+        warn: (...args) => console.warn(colors.bgYellow(`${param}:`), ...args),
+        error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
+      }
+    case 'error':
+      return {
+        error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
+      }
+    default:
+      return {
+        warn: (...args) => console.warn(colors.bgYellow(`${param}:`), ...args),
+        error: (...args) => console.error(colors.bgRed(`${param}:`), ...args),
+      }
+  }
 }
 
 module.exports = logger
