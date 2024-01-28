7937a0810deeb1d3c77c34099c50db44a63b34a7
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-28 12:46:37 +0200

    rebase logger function

--- config/default.js
+++ config/default.js
@@ -6,12 +6,12 @@ module.exports = {
   logColor,
   logger: (param) => {
     return {
-      info: (...args) => process.env.LOG_LEVEL==='info'
-        ? console.log(colors.bgWhite(`${param}:`), ...args):false,
-      warn: (...args) => process.env.LOG_LEVEL==='warn' || process.env.LOG_LEVEL==='info' || !process.env.LOG_LEVEL
-        ? console.error(colors.bgYellow(`${param}:`), ...args):false,
-      error: (...args) => process.env.LOG_LEVEL==='error' || process.env.LOG_LEVEL==='info' || process.env.LOG_LEVEL==='warn' || !process.env.LOG_LEVEL
-        ? console.error(colors.bgRed(`${param}:`), ...args):false,
+      info: (...args) => process.env.LOG_LEVEL === 'info'
+        ? console.log(colors.bgWhite(`${param}:`), ...args) : false,
+      warn: (...args) => process.env.LOG_LEVEL === 'warn' || process.env.LOG_LEVEL === 'info' || !process.env.LOG_LEVEL
+        ? console.error(colors.bgYellow(`${param}:`), ...args) : false,
+      error: (...args) => process.env.LOG_LEVEL === 'error' || process.env.LOG_LEVEL === 'info' || process.env.LOG_LEVEL === 'warn' || !process.env.LOG_LEVEL
+        ? console.error(colors.bgRed(`${param}:`), ...args) : false,
     }
   }
 }
