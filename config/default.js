add8cc250fca5ecd164f02af2989b826026f97d8
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-28 12:32:33 +0200

    rebase logger to config

--- config/default.js
+++ config/default.js
@@ -1,8 +1,17 @@
-const logger = require('../utils/logger');
+const colors = require('colors/safe');
 
 const logColor = process.env.COLORS_ENABLED && process.env.COLORS_ENABLED == 1 ? 'enabled' : 'disabled';
 
 module.exports = {
   logColor,
-  logger
+  logger: (param) => {
+    return {
+      info: (...args) => process.env.LOG_LEVEL==='info'
+        ? console.log(colors.bgWhite(`${param}:`), ...args):false,
+      warn: (...args) => process.env.LOG_LEVEL==='warn' || process.env.LOG_LEVEL==='info' || !process.env.LOG_LEVEL
+        ? console.error(colors.bgYellow(`${param}:`), ...args):false,
+      error: (...args) => process.env.LOG_LEVEL==='error' || process.env.LOG_LEVEL==='info' || process.env.LOG_LEVEL==='warn' || !process.env.LOG_LEVEL
+        ? console.error(colors.bgRed(`${param}:`), ...args):false,
+    }
+  }
 }
