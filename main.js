26e2d55d1c53cc50bec837cf3dd17cc8f84e95d7
AnnaKhiz <kukurika.anna@gmail.com>
2024-02-08 21:51:54 +0200

    change project. edit async function

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ require('dotenv').config();
 const config = require('config');
 const logger = require('./utils/logger');
 const colors = require('colors/safe');
-const fileSync = require('./file_sync');
-
-fileSync.start();
 
 config.isColorUsed ? colors.enable() : colors.disable();
 
-// logger('main').info('the script is running!');
-// logger('main').warn('this is warn');
-// logger('main').error('this is error');
+logger('main').info('the script is running!');
+logger('main').warn('this is warn');
+logger('main').error('this is error');
 
--- utils/logger.js
+++ utils/logger.js
@@ -1,16 +1,50 @@
 const config = require('config');
+const fs = require('fs');
+const path = require('path');
 const colors = require('colors/safe');
 
-const logger = (param) => {
+function checkDirectory() {
+	fs.mkdir(path.join('.', 'logs'), (error) => {
+		if (error && error.code === 'EEXIST') {
+			console.log('Directory already exist');
+		} else if (error) {
+			console.log(error);
+		}
+	})
+}
 
+checkDirectory();
+
+const writeStreamError = fs.createWriteStream(path.join('.', 'logs', 'error.log'), { encoding: 'utf8' });
+const writeStreamInfo = fs.createWriteStream(path.join('.', 'logs', 'info.log'), { encoding: 'utf8' });
+
+const logger = (param) => {
 	return {
-		info: (...args) => config.logLevel === 'info'
-			? console.log(colors.green(`${param}:`), ...args) : false,
-		warn: (...args) => config.logLevel === 'warn' || config.logLevel === 'info'
-			? console.error(colors.bgYellow(`${param}:`), ...args) : false,
-		error: (...args) => config.logLevel === 'error' || config.logLevel === 'info' || config.logLevel === 'warn'
-			? console.error(colors.bgRed(`${param}:`), ...args) : false,
+		info: (...args) => {
+			if (config.logLevel === 'info') {
+				console.log(colors.green(`${param}:`), ...args);
+			}
+			writeStreamInfo.write(`${param}: ${new Date().toISOString()} - ${args}\n`);
+		},
+		warn: (...args) => {
+			if (config.logLevel === 'warn' || config.logLevel === 'info') {
+				console.error(colors.bgYellow(`${param}:`), ...args);
+			}
+			writeStreamError.write(`${param}: ${new Date().toISOString()} - ${args}\n`);
+		},
+		error: (...args) => {
+			if (config.logLevel === 'error' || config.logLevel === 'info' || config.logLevel === 'warn') {
+				console.error(colors.bgRed(`${param}:`), ...args);
+			}
+			writeStreamError.write(`${param}: ${new Date().toISOString()} - ${args}\n`);
+		}
 	}
 }
+
+process.on('beforeExit', () => {
+	writeStreamError.end();
+	writeStreamInfo.end();
+})
+
 
 module.exports = logger
