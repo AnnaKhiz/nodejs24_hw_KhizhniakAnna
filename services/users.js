101f5e248b85e7cdc324d60137b205215fd9d294
AnnaKhiz <kukurika.anna@gmail.com>
2024-03-01 09:39:35 +0200

    add statuses

--- services/users.js
+++ services/users.js
@@ -11,7 +11,7 @@ async function getData(req, resp, next) {
 async function getDataById(req, resp, next) {
 	const { id } = req.params;
 	const result = await knex.select().from('USERS').where({ id }).first()
-	result ? resp.send(result) : resp.send({ "result": "no users found" });
+	result ? resp.send(result) : resp.status(404).send({ "result": "no users found" });
 }
 
 async function postData(req, resp, next) {
@@ -23,7 +23,7 @@ async function postData(req, resp, next) {
 async function deleteData(req, resp, next) {
 	const { id } = req.params;
 	const result = await knex('USERS').delete().where({ id });
-	result ? resp.send({ result: "successfully deleted" }) : resp.send({ "result": "no user found" })
+	result ? resp.status(204).send({ result: "successfully deleted" }) : resp.status(404).send({ "result": "no user found" })
 }
 
 module.exports = {
